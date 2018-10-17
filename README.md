3aa56761c70650a8e70a817d1c925da42975a84c
netfun81 <netfun81@yahoo.com>
2018-10-17 15:40:49 -0600

    added cycling through windows back with fixes

--- README.md
+++ README.md
@@ -3,25 +3,29 @@ minimal window manager
 
 Very minimal window manager using Windows Key + Mouse buttons for the following:
 
-Button1 - lower the current focused window exposing what is underneath
+Button1 - start dmenu application launcher.
 
-Button2 - make window fullscreen and move to right monitor
+Button2 - make focused window fullscreen and move to right monitor.
 
-Button3 - make window fullscreen and move to left monitor
+Button3 - make focused window fullscreen and move to left monitor.
+
+Button4 (scrollwheel) - cycle down through open programs.
+
+Button5 (scrollwheel) - cycle up through open programs. 
 
 
 to compile:  gcc -o kcmouse kcmouse.c -lX11
 
 
-I recommend to use xbindkeys and xdotool for launching, focus, and kill functions.   
+I recommend also using xbindkeys and xdotool for launching, focus, and kill functions.   
 
 #Sample .xbindkeysrc:
 
-	"dmenu_run"
-	mod4 + r
+	"firefox"
+	mod4 + w
 
 	"xdotool selectwindow windowfocus"
-	mod4 + x
+	mod4 + z
  
 	"xdotool selectwindow windowkill"
 	mod4 + q
--- kcmouse.c
+++ kcmouse.c
@@ -1,13 +1,12 @@
 #include <X11/Xlib.h>
-
 int main(void) {
     Display* display;
     Window root;
     XEvent event;
-    int unusedint; 
-    Window unusedwin;
-	Window focuswin;
-
+    int     unusedint; 
+    Window  unusedwin;
+    Window  focuswin;
+    
     display = XOpenDisplay(NULL);
     if(display == NULL)
     {
@@ -18,21 +17,32 @@ int main(void) {
            
         XGrabButton(display, 1, Mod4Mask, root, True, ButtonPressMask, GrabModeAsync, GrabModeAsync, None, None);
         XGrabButton(display, 2, Mod4Mask, root, True, ButtonPressMask, GrabModeAsync, GrabModeAsync, None, None);
-        XGrabButton(display, 3, Mod4Mask, root, True, ButtonPressMask, GrabModeAsync, GrabModeAsync, None, None);     
-
+        XGrabButton(display, 3, Mod4Mask, root, True, ButtonPressMask, GrabModeAsync, GrabModeAsync, None, None);
+        XGrabButton(display, 4, Mod4Mask, root, True, ButtonPressMask, GrabModeAsync, GrabModeAsync, None, None);
+        XGrabButton(display, 5, Mod4Mask, root, True, ButtonPressMask, GrabModeAsync, GrabModeAsync, None, None);      
     for(;;)
     {
 	XNextEvent(display, &event);
 	XQueryPointer(display, root, &unusedwin, &focuswin, &unusedint, &unusedint, &unusedint, &unusedint, &unusedint);
-		if(event.type == ButtonPress)
-        { 
-		if (event.xbutton.button == Button1) {
-            XLowerWindow(display, focuswin);  
-        } else if (event.xbutton.button == Button2) {		
+	if(event.type == ButtonPress)
+        {   
+        if (event.xbutton.button == Button1) {
+            system("dmenu_run");
+        }  else if (event.xbutton.button == Button2) {
             XMoveResizeWindow(display, focuswin, 1920, 0, 1920, 1080);
         } else if (event.xbutton.button == Button3) {		
             XMoveResizeWindow(display, focuswin, 0, 0, 1920, 1080);
-}  
+        }  else if (event.xbutton.button == Button4) {
+            XCirculateSubwindowsDown(display, root);
+            XQueryPointer(display, root, &unusedwin, &focuswin, &unusedint, &unusedint, &unusedint, &unusedint, &unusedint);
+            XSetInputFocus(display, focuswin, RevertToNone, CurrentTime);
+            XRaiseWindow(display, focuswin);
+        }  else if (event.xbutton.button == Button5) {
+            XCirculateSubwindowsUp(display, root);
+            XQueryPointer(display, root, &unusedwin, &focuswin, &unusedint, &unusedint, &unusedint, &unusedint, &unusedint);
+            XSetInputFocus(display, focuswin, RevertToNone, CurrentTime);
+            XRaiseWindow(display, focuswin);
+}
 }
 }
 }
