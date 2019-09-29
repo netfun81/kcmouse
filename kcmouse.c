9cd964ef7f74677b4be621c64d549fefffdc9329
netfun81 <netfun81@yahoo.com>
2019-09-29 01:37:43 -0600

    added polite closing of windows

--- kcmouse.c
+++ kcmouse.c
@@ -6,12 +6,21 @@ int main(void) {
     int     unusedint; 
     Window  unusedwin;
     Window  focuswin;
-    
+
+void delwin(focuswin)
+      { if (focuswin != None)
+	  { XEvent event;
+        event.xclient.type = ClientMessage;
+        event.xclient.window = focuswin;
+        event.xclient.message_type = XInternAtom(display, "WM_PROTOCOLS", True);
+        event.xclient.format = 32;
+        event.xclient.data.l[0] = XInternAtom(display, "WM_DELETE_WINDOW", False);
+        event.xclient.data.l[1] = CurrentTime;
+        XSendEvent(display, focuswin, False, NoEventMask, &event); }}
+       
     display = XOpenDisplay(NULL);
     if(display == NULL)
-    {
-        return 1;
-    }
+    { return 1; }
     
     root = DefaultRootWindow(display);
            
@@ -19,29 +28,32 @@ int main(void) {
         XGrabButton(display, 2, Mod4Mask, root, True, ButtonPressMask, GrabModeAsync, GrabModeAsync, None, None);
         XGrabButton(display, 3, Mod4Mask, root, True, ButtonPressMask, GrabModeAsync, GrabModeAsync, None, None);
         XGrabButton(display, 4, Mod4Mask, root, True, ButtonPressMask, GrabModeAsync, GrabModeAsync, None, None);
-        XGrabButton(display, 5, Mod4Mask, root, True, ButtonPressMask, GrabModeAsync, GrabModeAsync, None, None);      
+        XGrabButton(display, 5, Mod4Mask, root, True, ButtonPressMask, GrabModeAsync, GrabModeAsync, None, None);
+              
     for(;;)
-    {
-	XNextEvent(display, &event);
+    { XNextEvent(display, &event);
 	XQueryPointer(display, root, &unusedwin, &focuswin, &unusedint, &unusedint, &unusedint, &unusedint, &unusedint);
+	
 	if(event.type == ButtonPress)
-        {   
-           if (event.xbutton.button == Button1) 
-            system("firefox &");
-           else if (event.xbutton.button == Button2)
+        { if (event.xbutton.button == Button1) 
             system("dmenu_run &");
+           else if (event.xbutton.button == Button2)
+            delwin(event.xkey.subwindow);
            else if (event.xbutton.button == Button3)
-            XMoveResizeWindow(display, focuswin, 0, 0, 1920, 1080);   
-           else if (event.xbutton.button == Button4) { 
-            XCirculateSubwindowsDown(display, root);
-            XQueryPointer(display, root, &unusedwin, &focuswin, &unusedint, &unusedint, &unusedint, &unusedint, &unusedint);
-            XSetInputFocus(display, focuswin, RevertToNone, CurrentTime);
-            XRaiseWindow(display, focuswin);         }
-           else if (event.xbutton.button == Button5) {
-            XCirculateSubwindowsUp(display, root);
-            XQueryPointer(display, root, &unusedwin, &focuswin, &unusedint, &unusedint, &unusedint, &unusedint, &unusedint);
-            XSetInputFocus(display, focuswin, RevertToNone, CurrentTime);
-            XRaiseWindow(display, focuswin);         }
+           { if (focuswin != None)
+		   { XMoveResizeWindow(display, focuswin, 0, 0, 1920, 1080); }}			      
+           else if (event.xbutton.button == Button4)
+           { if (focuswin != None)
+		   { XCirculateSubwindowsDown(display, root);
+             XQueryPointer(display, root, &unusedwin, &focuswin, &unusedint, &unusedint, &unusedint, &unusedint, &unusedint);
+             XSetInputFocus(display, focuswin, RevertToNone, CurrentTime);
+             XRaiseWindow(display, focuswin); }}
+           else if (event.xbutton.button == Button5)
+           { if (focuswin != None)
+		   { XCirculateSubwindowsUp(display, root);
+             XQueryPointer(display, root, &unusedwin, &focuswin, &unusedint, &unusedint, &unusedint, &unusedint, &unusedint);
+             XSetInputFocus(display, focuswin, RevertToNone, CurrentTime);
+             XRaiseWindow(display, focuswin); }}
 }
 }
 }
